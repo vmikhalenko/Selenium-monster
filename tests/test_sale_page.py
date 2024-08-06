31671e328800ff21bde7773178b66ababf6186d9
vmikhalenko <mikhalenko2000@inbox.ru>
2024-08-06 21:19:05 +0300

    Updating test_sale_page.py

--- tests/test_sale_page.py
+++ tests/test_sale_page.py
@@ -2,7 +2,7 @@ import pytest
 import allure
 
 @allure.feature('Header text')
-@pytest.mark.regression
+@pytest.mark.extended
 def test_header_text(sale_page):
     sale_page.open_page()
     sale_page.check_page_header('Sale')
